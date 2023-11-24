24f01c9d2a0d2077bfbee01c8efd201db148371d
稚子 <1973688584@qq.com>
2023-11-24 09:11:50 +0800

    完善UART发送数据的描述

--- 2.STM32/3.UART串口DMA发送数据/Serial.c
+++ 2.STM32/3.UART串口DMA发送数据/Serial.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include <stdarg.h>
 
+// 重定向fputc函数，使用printf()发送数据
+int fputc(int ch, FILE *f)
+{
+	// 参数1：串口句柄，参数2：要发送的数据；参数3：要发生数据的长度；参数4：超时等待时间
+	HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 100);
+	return ch;
+}
+
 // 堵塞模式
 void log(const char *format, ...)
 {
@@ -28,7 +36,7 @@ void log_IT(const char *format, ...)
 	// 等待上次的数据发送完成，避免新的数据覆盖正在传输的数据，导致混乱
 	while (HAL_UART_GetState(&huart1) == HAL_UART_STATE_BUSY_TX)
 	{
-		// Wait for DMA transfer to complete
+		// Wait for transfer to complete
 	}
 
 	HAL_UART_Transmit_IT(&huart1, (uint8_t *)strBuf, strlen(strBuf));
